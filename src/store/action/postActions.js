fbb7e61651d78285eb7d1fa9003cf3c0993a76d8
yonittsairi <yonittsairi@gmail.com>
2021-05-23 12:56:03 +0300

    pages3

--- src/store/action/postActions.js
+++ src/store/action/postActions.js
@@ -1,23 +1,23 @@
 import { eventiService } from "../../service/postService.js"
 
-export function loadEventis(filterBy) {
+export function loadPosts(filterBy) {
     return async dispatch => {
         try {
             const eventies = await eventiService.query(filterBy)
-            dispatch({ type: 'SET_EVENTIES', eventies })
+            dispatch({ type: 'SET_POSTS', eventies })
         } catch (err) {
-            console.log('Eventi Actions: err in load', err)
+            console.log('Post Actions: err in load', err)
         }
 
     }
 }
-export function loadUserEventis(filterBy) {
+export function loadUserPosts(filterBy) {
     return async dispatch => {
         try {
             const eventies = await eventiService.queryByUser(filterBy)
-            dispatch({ type: 'SET_EVENTIES', eventies })
+            dispatch({ type: 'SET_POSTS', eventies })
         } catch (err) {
-            console.log('Eventi Actions: err in load', err)
+            console.log('Post Actions: err in load', err)
         }
 
     }
@@ -25,11 +25,11 @@ export function loadUserEventis(filterBy) {
 
 
 
-export function removeEventi(eventiId) {
+export function removePost(eventiId) {
     return async dispatch => {
         try {
             await eventiService.remove(eventiId)
-            dispatch({ type: 'REMOVE_EVENTI', eventiId })
+            dispatch({ type: 'REMOVE_POST', eventiId })
         }
         catch (err) {
             alert('OOPs, try again');
@@ -38,12 +38,12 @@ export function removeEventi(eventiId) {
     }
 }
 
-export function editEventi(eventi) {
+export function editPost(eventi) {
     return async dispatch => {
         try {
-            const editedEventi = await eventiService.update(eventi)
-            console.log("editing  action ", editedEventi);
-            dispatch({ type: 'EDIT_EVENTI', eventi: editedEventi })
+            const editedPost = await eventiService.update(eventi)
+            console.log("editing  action ", editedPost);
+            dispatch({ type: 'EDIT_POST', eventi: editedPost })
         } catch (err) {
             alert('OOPs, try again to edit');
             console.log('ERR:', err);
@@ -51,11 +51,11 @@ export function editEventi(eventi) {
 
     }
 }
-export function addEventi(eventi) {
+export function addPost(eventi) {
     return async dispatch => {
         try {
-            const addEventi = await eventiService.add(eventi)
-            dispatch({ type: 'ADD_EVENTI', eventi: addEventi })
+            const addPost = await eventiService.add(eventi)
+            dispatch({ type: 'ADD_POST', eventi: addPost })
         } catch (err) {
             alert('OOPs, try again');
             console.log('ERR:', err);
